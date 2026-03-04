c3ec2740e6e2f06f83ab4ec311856bdd6115847b
Jason Wang <jason.wang@ocvts.org>
2026-03-04 10:10:55 -0500

    Add settings overlay animations and hook menu script
    Attach settings.gd to the main menu and add animated settings overlay. main_menu.tscn: add Script and Texture ext_resources, create Animation subresources and an AnimationLibrary (RESET, popup), adjust ColorRect and PanelContainer anchors, add a TextureRect for background, and assign the AnimationLibrary to the AnimationPlayer. settings.gd: replace the previous ready/connect logic with onready node references and change _on_pressed() to show the settings layer and play the "popup" animation (instead of changing scenes). This implements an in-place animated settings overlay for the main menu.

--- settings.gd
+++ settings.gd
@@ -1,7 +1,11 @@
 extends Button
 
-func _ready():
-	pressed.connect(_on_pressed)
+# Paths based on your uploaded scene tree
+@onready var settings_layer = $"../../CanvasLayer"
+@onready var anim_player = $"../../CanvasLayer/ColorRect/PanelContainer/AnimationPlayer"
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Settings.tscn")
+	# 1. Make the layer visible first
+	settings_layer.show()
+	# 2. Play the animation
+	anim_player.play("popup")
