f8dc24c930cfcbfa388889722aa5d3ce768dc8c0
Ryan Madensky <madensrp@gmail.com>
2026-03-13 10:16:44 -0400

    Lives System
    Added health bar, enemies deal damage to health

---
+++ loss_conditions.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var lives = 100
+
+@onready var label = $"../Lives/LivesLabel"
+
+func update_label():
+	label.text = str(lives)
+func _ready():
+	update_label()
+
+func add_lives(amount: int):
+	lives += amount
+	update_label()
+
+func spend_lives(amount: int):
+	lives -= amount
+	update_label()
+
+
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		add_lives(10)
