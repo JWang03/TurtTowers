6d81e14551161413dcbbb525c36570c262115b6a
Ryan Madensky <madensrp@gmail.com>
2026-03-13 12:11:40 -0400

    Added Loss System

--- loss_conditions.gd
+++ loss_conditions.gd
@@ -17,8 +17,12 @@ func spend_lives(amount: int):
 	lives -= amount
 	update_label()
 
-
-	
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		add_lives(10)
+func is_alive(lives):
+	if lives<=0:
+		return false
+	else:
+		return true
+		
