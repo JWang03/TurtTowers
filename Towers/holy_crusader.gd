5e13d02b2be5add94ba82576e91b557acc3ab42d
Ryan Madensky <madensrp@gmail.com>
2026-04-23 12:16:16 -0400

    bug fix

--- Towers/holy_crusader.gd
+++ Towers/holy_crusader.gd
@@ -1,64 +1,53 @@
 extends Node2D
-
-@export var fire_rate: float = 0.0001
+@export var fire_rate: float = .0001
 @export var damage: int = 100
 @export var cost: int = 20
 @export var is_placed: bool = false
 @onready var range_area = $Range
 @onready var collision_shape = $Range/CollisionShape2D
-@onready var timer = $Timer
 @onready var starter = get_node("/root/Game/UI/Start_Pause/PlayButton")
 var beam_scene = preload("res://towers/holybeam.tscn")
 var targets_in_range: Array = []
 var rng = RandomNumberGenerator.new()
+var time_since_last_shot: float = 0.0
 
 func _ready():
 	rng.randomize()
-	timer.wait_time = fire_rate
-	
 	range_area.body_entered.connect(_on_zombie_entered)
 	range_area.body_exited.connect(_on_zombie_exited)
-	timer.timeout.connect(_on_timer_timeout)
+
+func _process(delta):
+	if not is_placed or targets_in_range.is_empty():
+		return
+	if starter.playing == true:
+		time_since_last_shot += delta
+		if time_since_last_shot >= fire_rate:
+			smite()
+			time_since_last_shot = 0.0
 
 func _on_zombie_entered(body):
 	if is_placed:
 		if body.is_in_group("zombies"):
 			targets_in_range.append(body)
-			if timer.is_stopped():
-				smite()
-				timer.start()
 
 func _on_zombie_exited(body):
 	if is_placed:
 		if body in targets_in_range:
 			targets_in_range.erase(body)
-		
-		if targets_in_range.is_empty():
-			timer.stop()
-
-func _on_timer_timeout():
-	if not targets_in_range.is_empty():
-		smite()
-	else:
-		timer.stop()
 
 func smite():
-	if starter.playing == true:
-		var shape = collision_shape.shape
-		if shape is CircleShape2D:
-			var radius = shape.radius
-			
-			var angle = rng.randf_range(0, TAU)
-			var dist = sqrt(rng.randf()) * radius
-			var offset = Vector2(cos(angle), sin(angle)) * dist
-			var spawn_pos = global_position + offset
-			
-			spawn_beam(spawn_pos)
+	var shape = collision_shape.shape
+	if shape is CircleShape2D:
+		var radius = shape.radius
+		var angle = rng.randf_range(0, TAU)
+		var dist = sqrt(rng.randf()) * radius
+		var offset = Vector2(cos(angle), sin(angle)) * dist
+		var spawn_pos = global_position + offset
+		spawn_beam(spawn_pos)
 
 func spawn_beam(pos: Vector2):
 	var beam = beam_scene.instantiate()
 	get_tree().current_scene.add_child(beam)
 	beam.global_position = pos
-	
 	if beam.has_method("set_damage"):
 		beam.set_damage(damage)
